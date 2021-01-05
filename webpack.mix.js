ddddc48e4c4833db82d05929394a7778cd8a4608
Valeria <valeriaablanco@yahoo.com>
2021-01-05 12:23:17 -0300

    Modificación de webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
-mix.browserSync('http://emersis_back.test/');
+mix.browserSync('http://chat-ea-web-BACK.test/');
